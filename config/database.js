f3c5107645f29b67235a63ff7d108d9e5fef0a63
zubaiur <zahidzahidhusssain@gmail.com>
2024-12-01 19:24:32 +0500

    Updated project files

---
+++ config/database.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'mysql',
+    connection: {
+      host: env('DATABASE_HOST', 'mysql.railway.internal'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'railway'),
+      user: env('DATABASE_USERNAME', 'root'),
+      password: env('DATABASE_PASSWORD', 'DUNHqkFPKTqTllToMWggbADRlnIAaUeg'),
+          ssl: false, 
+    },
+  },
+});
